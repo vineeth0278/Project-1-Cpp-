b6ebebb7b43205b50bad41f44861456120c67016
vineeth0278 <69383575+vineeth0278@users.noreply.github.com>
2020-12-16 14:59:06 +0530

    Add files via upload

---
+++ C++ multiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a,b;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>a>>b;
+	cout<<"Product="<<a*b;
+	return 0;
+	
+}
---
+++ C++ size of datatypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout<<"size of integer is"<<sizeof(int)<<"bytes"<<endl;
+cout<<"size of float is"<<sizeof(float)<<"bytes"<<endl;
+cout<<"size of char is"<<sizeof(char)<<"bytes"<<endl;
+cout<<"size of double is"<<sizeof(double)<<"bytes"<<endl;
+return 0;
+}
